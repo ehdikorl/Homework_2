0d49fd117db05c026673ec7d27987e139cefd47e
ehdikorl <117590479+ehdikorl@users.noreply.github.com>
2022-12-02 23:25:32 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int ns = 100;
-        int sp = 1501;
-        int is;
+        int nachalnaya_summa = 100;
+        int summa_popolneniya = 1501;
+        int itogovaya_summa;
 
-        if(sp < 1000) {
-            is = ns + sp;
-            System.out.println("Бонусов нет. Итоговый счет " + is);
+        if (summa_popolneniya < 1000) {
+            itogovaya_summa = nachalnaya_summa + summa_popolneniya;
+            System.out.println("Бонусов нет. Итоговый счет " + itogovaya_summa);
         } else {
-            is = ns + sp + sp/100;
-            System.out.println("Итоговый счет " + is + ". Зачислено бонусов " + sp/100);
+            itogovaya_summa = nachalnaya_summa + summa_popolneniya + summa_popolneniya / 100;
+            System.out.println("Итоговый счет " + itogovaya_summa + ". Зачислено бонусов " + summa_popolneniya / 100);
         }
 
     }
